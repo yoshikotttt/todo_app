886ce83295037cb8dfeec6ff0d1e902f978af3dc
yoshikotttt <myqic.n@gmail.com>
2023-04-26 19:00:24 +0900

    first_commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Swal.fire({
+//     title: 'ほぞんしたよ！',
+//     text: 'きょうもいちにちおつかれさまでした',
+//     imageUrl:"img/prcure.jpg",
+//     imageWidth: 400,
+//     imageHeight: 200,
+//     imageAlt: 'Custom image',
+//   })
